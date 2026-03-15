bd998bebdcd3d774b9aec7d0299d316024f08996
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-03-15 14:05:12 +0530

    feat: add UI components for card, dropdown menu, field, input, label, and separator
    - Implemented Card component with subcomponents: CardHeader, CardTitle, CardDescription, CardAction, CardContent, and CardFooter.
    - Created DropdownMenu component with various subcomponents including DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, and DropdownMenuCheckboxItem.
    - Developed Field component with FieldSet, FieldLegend, FieldGroup, FieldLabel, FieldDescription, FieldError, and FieldSeparator.
    - Added Input component for form inputs.
    - Introduced Label component for form labels.
    - Created Separator component for visual separation in forms.
    - Implemented LoginForm and SignupForm components for user authentication.
    - Added ThemeToggle component for switching themes.
    - Established ThemeProvider for managing theme state across the application.
    - Created routes for login and signup pages.

--- src/components/web/navbar.tsx
+++ src/components/web/navbar.tsx
@@ -1,12 +1,18 @@
+import { Link } from "@tanstack/react-router"
+import { buttonVariants } from "../ui/button"
+import { ThemeToggle } from "./theme-toggle"
+
 const Navbar = () => {
-  return <nav className="sticky z-50 top-0 border-b bg-amber-50 backdrop-blur-2xl">
+  return <nav className="sticky z-50 top-0 border-b bg-background/95 backdrop-blur supports-backdrop-filterbg-background/60:">
     <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         <div className="flex items-center gap-2">
             <img src="https://tanstack.com/images/logos/logo-color-banner-600.png" alt="Ransatck Start Logo" className="size-8" />
             <h1 className="text-lg font-semibold">Tanstack Start</h1>
         </div>
         <div className="flex items-center gap-3">
-            <button className="text-white bg-red-500 px-3 py-1 rounded-full">Sign Up</button>
+            <ThemeToggle />
+            <Link to="/login" className={buttonVariants({variant: "secondary"})}>Login</Link>
+            <Link to="/signup" className={buttonVariants()}>Get Started</Link>
         </div>
     </div>
   </nav>
