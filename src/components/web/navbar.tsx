27993dc1d2726d6d99251346c10a4b73e61e82fb
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-03-15 11:31:36 +0530

    feat: initial commit

---
+++ src/components/web/navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return <nav className="sticky z-50 top-0 border-b bg-amber-50 backdrop-blur-2xl">
+    <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
+        <div className="flex items-center gap-2">
+            <img src="https://tanstack.com/images/logos/logo-color-banner-600.png" alt="Ransatck Start Logo" className="size-8" />
+            <h1 className="text-lg font-semibold">Tanstack Start</h1>
+        </div>
+        <div className="flex items-center gap-3">
+            <button className="text-white bg-red-500 px-3 py-1 rounded-full">Sign Up</button>
+        </div>
+    </div>
+  </nav>
+}
+
+export default Navbar
