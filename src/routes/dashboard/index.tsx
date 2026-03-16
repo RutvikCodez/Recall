8e960081a24a50ba997817e9756d8455de686feb
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-03-16 10:53:19 +0530

    user will automaticallly redirect to "/dashboard/items" after logged in or whenever goes to dashboard

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,11 +1,7 @@
-import { getSessionFn } from '#/data/session'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard/')({
-  component: RouteComponent,
-  loader: () => getSessionFn()
+  beforeLoad: () => {
+    throw redirect({ to: '/dashboard/items' })
+  },
 })
-
-function RouteComponent() {
-  return <div>hello</div>
-}
